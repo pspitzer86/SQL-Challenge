8fa2940361d6681472e06ed03cf6c4423f92e515
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-01-31 17:11:33 -0700

    fourth query

--- EmployeeSQL/Output/SQL_Data_Analysis.sql
+++ EmployeeSQL/Output/SQL_Data_Analysis.sql
@@ -34,6 +34,15 @@ SELECT d.dept_no,
 
 -- List the department of each employee with the following information:
 -- employee number, last name, first name, and department name.
+SELECT e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+	FROM employees e
+	INNER JOIN dept_emp de ON
+	e.emp_no = de.emp_no
+	INNER JOIN departments d ON
+	d.dept_no = de.dept_no;
 
 
 -- List first name, last name, and sex for employees whose
