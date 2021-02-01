7e4a4197c9bf7cae9715bb77ae36c058fc076917
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-01-31 17:13:44 -0700

    fifth query

--- EmployeeSQL/Output/SQL_Data_Analysis.sql
+++ EmployeeSQL/Output/SQL_Data_Analysis.sql
@@ -47,6 +47,13 @@ SELECT e.emp_no,
 
 -- List first name, last name, and sex for employees whose
 -- first name is "Hercules" and last names begin with "B."
+SELECT first_name,
+	last_name,
+	sex
+	FROM employees
+	WHERE first_name = 'Hercules' AND
+	last_name LIKE 'B%';
+	
 
 
 -- List all employees in the Sales department,
