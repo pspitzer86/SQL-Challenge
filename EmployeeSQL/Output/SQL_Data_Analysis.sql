fa812f56092fc7e13986140d85b50dd9795dc15a
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-01-31 17:22:27 -0700

    eighth query

--- EmployeeSQL/Output/SQL_Data_Analysis.sql
+++ EmployeeSQL/Output/SQL_Data_Analysis.sql
@@ -86,3 +86,6 @@ SELECT e.emp_no,
 
 -- In descending order, list the frequency count of employee last names,
 -- i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) FROM employees
+GROUP BY last_name
+ORDER BY COUNT(last_name) desc;
