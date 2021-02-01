a122d7cab727b249f6cdd9047b7b8c7087a813ca
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-01-31 17:15:41 -0700

    sixth query

--- EmployeeSQL/Output/SQL_Data_Analysis.sql
+++ EmployeeSQL/Output/SQL_Data_Analysis.sql
@@ -55,9 +55,18 @@ SELECT first_name,
 	last_name LIKE 'B%';
 	
 
-
 -- List all employees in the Sales department,
 -- including their employee number, last name, first name, and department name.
+SELECT e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+	FROM employees e
+	INNER JOIN dept_emp de ON
+	e.emp_no = de.emp_no
+	INNER JOIN departments d ON
+	d.dept_no = de.dept_no
+	WHERE d.dept_name = 'Sales';
 
 
 -- List all employees in the Sales and Development departments,
