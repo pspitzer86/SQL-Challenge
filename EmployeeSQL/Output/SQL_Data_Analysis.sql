c58788d89ff2d9f3dd8afd7c5a31c1ba5e4f2dda
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-01-31 17:17:52 -0700

    seventh query

--- EmployeeSQL/Output/SQL_Data_Analysis.sql
+++ EmployeeSQL/Output/SQL_Data_Analysis.sql
@@ -71,6 +71,17 @@ SELECT e.emp_no,
 
 -- List all employees in the Sales and Development departments,
 -- including their employee number, last name, first name, and department name.
+SELECT e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+	FROM employees e
+	INNER JOIN dept_emp de ON
+	e.emp_no = de.emp_no
+	INNER JOIN departments d ON
+	d.dept_no = de.dept_no
+	WHERE d.dept_name = 'Sales' OR
+	d.dept_name = 'Development';
 
 
 -- In descending order, list the frequency count of employee last names,
